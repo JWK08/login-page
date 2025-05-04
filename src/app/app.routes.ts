04df2f9757426c3327e13981ccb6437ac4986166
José Wilken <josewilken1@gmail.com>
2025-05-04 12:36:06 -0300

    Front end finalizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,21 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { DefaultLoginLayoutComponent } from './components/default-login-layout/default-login-layout.component';
+import { SignUpComponent } from './pages/signup/signup.component';
+import { UserComponent } from './pages/user/user.component';
+import { AuthGuard } from './services/auth-guard.service';
 
 export const routes: Routes = [
     {
-        path: "login",
-        component: LoginComponent 
+        path: "",
+        component: LoginComponent
+    },
+    {
+        path: "signup",
+        component: SignUpComponent
+    },
+    {
+        path: "user",
+        component: UserComponent,
+        canActivate: [AuthGuard]
     }
 ];
